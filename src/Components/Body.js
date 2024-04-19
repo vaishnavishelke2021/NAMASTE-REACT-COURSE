186c3b00580d2caa3a4d622d90139576cbda863b
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-04-19 13:27:41 +0530

    used the swiggy API

--- src/Components/Body.js
+++ src/Components/Body.js
@@ -1,11 +1,22 @@
 import "./Body.css";
 import RestaurantCard from "./RestaurantCard";
-import resList from "../utils/mockData";
-import { useState } from "react";
+// import resList from "../utils/mockData";
+import { useState, useEffect } from "react";
 
 
 const Body = () => {
-  const [listOfRes, setListOfRes] = useState(resList);
+  const [listOfRes, setListOfRes] = useState([]);
+
+  useEffect(()=>{
+      fetchData();
+  }, [])
+
+  const fetchData = async ()=>{
+    const data = await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=19.9974533&lng=73.78980229999999");
+    const json = await data.json();
+    console.log(json);
+    setListOfRes(json.data.success.cards[1].gridWidget.gridElements.infoWithStyle.restaurants)
+  }
 
   const handleFilter = () => {
     const filteredRes = listOfRes.filter((res) => res.info.avgRating > 4);
