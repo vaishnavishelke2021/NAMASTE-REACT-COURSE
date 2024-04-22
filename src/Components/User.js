2ba0623dadb0531db94cb32ce588a4da0b6254b1
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-04-22 20:46:17 +0530

    fetched api in class component

--- src/Components/User.js
+++ src/Components/User.js
@@ -5,17 +5,39 @@ class User extends React.Component{
     constructor(props){
         super(props);
         console.log(props);
+        console.log('constructor is rendered');
+
+        this.state = {
+            userInfo: {
+                name: "username",
+            blog: "blog link",
+            avatar_url: "dummy-avatar"
+            }  //another state
+        };
     }
+
+    async componentDidMount() {
+        const data = await fetch("https://api.github.com/users/vaishnavishelke2021");
+        const json = await data.json();
+
+        this.setState({
+            userInfo: json,
+        })
+        console.log(json);
+      }
+      
 
     
 
     render(){
-        const {name, location} = this.props;
-        
+        const {name, blog, avatar_url} = this.state.userInfo;
+        console.log("render method is called");
+
         return (
             <>
+            <img src={avatar_url} alt="" />
               <h1>Name : {name}</h1>
-              <h2>Location: {location}</h2>
+              <h2>Blog: {blog} </h2>
               <a href="https://vaishnavi-shelke.netlify.app/">Portfolio</a>
             </>
         )
