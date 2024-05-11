56bb005713e0acd03612bb0a205991e2665809d7
VaishnaviShelke <vaishnavishelke7777@gmail.com>
2024-05-11 11:52:19 +0530

    added test cases for login-logout button

--- src/Components/__tests__/Header.test.js
+++ src/Components/__tests__/Header.test.js
@@ -1,4 +1,4 @@
-import { render, screen } from "@testing-library/react";
+import { fireEvent, render, screen } from "@testing-library/react";
 import Header from "../Header";
 import { Provider } from "react-redux";
 import appStore from "../../utils/appStore";
@@ -16,4 +16,34 @@ test("should load Header with login button", () => {
 
   const loginButton = screen.getByRole("button", {name: "Login"});
   expect(loginButton).toBeInTheDocument();
+});
+
+
+test("should load Header with cart items", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const cartItems = screen.getByText(/(0)/);   // /../ this is regex , using this we don't need to write complete string
+  expect(cartItems).toBeInTheDocument();
+});
+
+
+test("should change login button to logout on click", () => {
+  render(
+    <BrowserRouter>
+      <Provider store={appStore}>
+        <Header />
+      </Provider>
+    </BrowserRouter>
+  );
+
+  const loginButton = screen.getByRole("button", {name: "Login"});
+  fireEvent.click(loginButton);                                          //fireEvent to fire an event
+  const logoutButton = screen.getByRole("button", {name: "Logout"});
+  expect(logoutButton).toBeInTheDocument();
 });
